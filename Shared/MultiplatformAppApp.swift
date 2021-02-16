f95c865ec3936719951c268e9faaa116449fcbad
Andrés Vazquez <andavazgar.dev@gmail.com>
2021-02-16 00:20:21 -0500

    Initial Commit

---
+++ Shared/MultiplatformAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformAppApp.swift
+//  Shared
+//
+//  Created by Andres Vazquez on 2021-02-16.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
